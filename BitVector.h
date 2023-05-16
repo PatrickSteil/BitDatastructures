c0c4d09070733f64d5d920032bd77958f3c8cd3c
Patrick Steil <np254@stud.uni-heidelberg.de>
2023-05-16 17:27:20 +0200

    sped up the reading

--- BitVector.h
+++ BitVector.h
@@ -1,8 +1,7 @@
+#include "Assert.h"
+#include <cmath> // ceil
 #include <iostream>
 #include <vector>
-#include <cmath> // ceil
-#include "Assert.h"
-
 
 namespace BIV {
 class BitVector {
@@ -10,44 +9,56 @@ class BitVector {
     using BLOCK = uint64_t;
 
 public:
-	BitVector(size_t numBits = 64) : 
-		numBits(numBits),
-		shiftBlockSize(6) {
-			assertmsg(numBits >= 0, "numBits should not be negative");
-		bit_vector.assign(std::ceil(numBits / 64.0), 0);
-	};
-	
-	void printInfo() const noexcept {
-	    std::cout << "BitVector Stats:" << std::endl;
-	    std::cout << "Number of bits:      " << (int) numBits << std::endl;
-	    std::cout << "Number of uint64_t:  " << (int) bit_vector.size() << std::endl;
-	}
-	
-	bool operator [](int i) const {
-		assertmsg(0 <= i && (size_t) i < numBits, "index out of bounds!");
-	    BLOCK block = bit_vector[i >> shiftBlockSize];
-	    return (block >> (i & 63)) &1ULL;
-	}
-	
-	void set(int i) {
-		assertmsg(0 <= i && (size_t) i < numBits, "index out of bounds!");
-	    BLOCK& block = bit_vector[i >> shiftBlockSize];
-	    block |= (1 << (i & 63));
-	}
-	
-	void unset(int i) {
-		assertmsg(0 <= i && (size_t) i < numBits, "index out of bounds!");
-	    BLOCK& block = bit_vector[i >> shiftBlockSize];
-	    block &= ~(1 << (i & 63));
-	}
-	
-	size_t size() const noexcept {
-	    return numBits;
-	}
+    BitVector(size_t numBits = 64)
+        : numBits(numBits)
+    {
+        assertmsg(numBits >= 0, "numBits should not be negative");
+        bit_vector.assign(std::ceil(numBits / 64.0), 0);
+    };
+
+    void printInfo() const noexcept
+    {
+        std::cout << "BitVector Stats:" << std::endl;
+        std::cout << "Number of bits:      " << (int)numBits << std::endl;
+        std::cout << "Number of uint64_t:  " << (int)bit_vector.size() << std::endl;
+    }
+
+    size_t totalSizeByte() const noexcept
+    {
+        size_t total(0);
+        total += sizeof(numBits);
+        total += sizeof(bit_vector);
+        return total;
+    }
+
+    bool operator[](int i) const
+    {
+        assertmsg(0 <= i && (size_t)i < numBits, "index out of bounds!");
+        BLOCK block = bit_vector[i >> 6];
+        return (block >> (i & 63)) & 1ULL;
+    }
+
+    void set(int i)
+    {
+        assertmsg(0 <= i && (size_t)i < numBits, "index out of bounds!");
+        BLOCK& block = bit_vector[i >> 6];
+        block |= (1 << (i & 63));
+    }
+
+    void unset(int i)
+    {
+        assertmsg(0 <= i && (size_t)i < numBits, "index out of bounds!");
+        BLOCK& block = bit_vector[i >> 6];
+        block &= ~(1 << (i & 63));
+    }
+
+    size_t size() const noexcept
+    {
+        return numBits;
+    }
 
 private:
-	size_t numBits;
-	int shiftBlockSize;
-	std::vector<BLOCK> bit_vector;
+    size_t numBits;
+    std::vector<BLOCK> bit_vector;
 };
 }
--- ads_programm.cpp
+++ ads_programm.cpp
@@ -1,13 +1,113 @@
+#include <chrono>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
 #include "EliasFano.h"
 
-int main() {
+int main(int argc, char const* argv[])
+{
+    using std::chrono::duration;
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
 
-	std::vector<int> numbers = {1, 2, 4, 6, 32, 33, 40, 56};
-	EF::EliasFano ef(numbers, 64);
+    if (argc != 4) {
+        std::cout << "Not enough arguments!\nExample call: ads_programm [pd|rmq] "
+                     "eingabe_datei ausgabe_datei"
+                  << std::endl;
+        return (-1);
+    }
 
-	ef.printInfo();
-	return 0;
+    auto t1 = high_resolution_clock::now();
+
+    // read the input file
+    std::vector<int> numbers;
+    std::vector<int> queries;
+    std::ifstream inputFile(argv[2]);
+
+    int n;
+    inputFile >> n;
+    numbers.reserve(n);
+
+    int lc = 0;
+    int curNumber = 0;
+    char buf[2048];
+    do {
+        inputFile.read(buf, sizeof(buf));
+        int k = inputFile.gcount();
+        for (int i = 0; i < k; ++i) {
+            switch (buf[i]) {
+            case '\r':
+                break;
+            case '\n':
+                curNumber = 0;
+                lc++;
+                break;
+            case ' ':
+                curNumber = 0;
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                curNumber = 10 * curNumber + buf[i] - '0';
+                break;
+            default:
+                std::cerr << "Bad format\n";
+            }
+        }
+        numbers.push_back(curNumber);
+        --n;
+    } while (n >= 0);
+
+    do {
+        inputFile.read(buf, sizeof(buf));
+        int k = inputFile.gcount();
+        for (int i = 0; i < k; ++i) {
+            switch (buf[i]) {
+            case '\r':
+                break;
+            case '\n':
+                curNumber = 0;
+                lc++;
+                break;
+            case ' ':
+                curNumber = 0;
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                curNumber = 10 * curNumber + buf[i] - '0';
+                break;
+            default:
+                std::cerr << "Bad format\n";
+            }
+        }
+        queries.push_back(curNumber);
+        --n;
+    } while (inputFile);
+
+    EF::EliasFano ef(numbers);
+
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    std::cout << "RESULT algo=" << argv[1]
+              << " namepatrick_steil time=" << ms_int.count()
+              << " space=" << (ef.totalSizeByte() << 3) << std::endl;
+    return 0;
 }
